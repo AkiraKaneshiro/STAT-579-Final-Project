71915714fc522fca2d9525878d2005b02a75fffb
epwalsh <epwalsh10@gmail.com>
2014-11-20 14:42:17 -0600

    Organizing business data
    Cleaned data a bit - separated restaurant data from everything else.
    Then broke down restaurant data into breakfast, lunch, dinner, and
    late-night establishments.

--- 01_Data_clean/Pete_data_clean.R
+++ 01_Data_clean/Pete_data_clean.R
@@ -1,1 +1,24 @@
 # Initial data cleaning
+
+business <- read.csv("Classes and Work/yelp/business.csv")
+
+# Let's focus on data for restaurants 
+idx <- grep("restaurant", business$categories, ignore.case=T)
+food <- business[idx,]
+rm(business)
+
+names(food)
+unique(food$type)
+unique(food$state)
+summary(food$stars)
+summary(food$review_count)
+head(food$open)
+summary(food$open)
+head(food$name)
+str(food)
+unique(food$neighborhoods)
+unique(food$city)
+
+food$attributes_Good.For <- as.character(food$attributes_Good.For)
+idx <- grep("lunch': True", food$attributes_Good.For)
+lunch <- food[idx,]
