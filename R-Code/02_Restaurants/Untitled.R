8372aa3a0b70c2191711bb13554d8bc1086ff6f3
epwalsh <epwalsh10@gmail.com>
2014-11-21 16:51:14 -0600

    stuff

---
+++ R-Code/02_Restaurants/Untitled.R
@@ -0,0 +1,4 @@
+food <- read.csv("Github/STAT-579-Final-Project/Data/Restaurants/food.csv")
+bfast <- read.csv("Github/STAT-579-Final-Project/Data/Restaurants/bfast.csv")
+lunch <- read.csv("Github/STAT-579-Final-Project/Data/Restaurants/lunch.csv")
+dinner <- read.csv("Github/STAT-579-Final-Project/Data/Restaurants/dinner.csv")
